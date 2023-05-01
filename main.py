b1f7961d2301b9d3fa769874f2c014be13d08fa3
Oleh777 <43369480+Oleh777@users.noreply.github.com>
2023-05-02 02:27:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
+from pygame.constants import QUIT
 import sys
 
 pygame.init()
 
 FPS = pygame.time.Clock()
 
-HEIGHT = 800
-WIDTH = 1200
+HEIGHT = 600
+WIDTH = 1000
 COLOR_WHITE = (255,255,255)
 COLOR_BLACK = (0, 0, 0)
 
@@ -19,22 +20,26 @@ player.fill((COLOR_WHITE))
 player_rect = player.get_rect()
 player_speed = [1, 1]
 
-playing = True
 while True: 
-    FPS.tick(120)
-
+    FPS.tick(620)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            playing = False
-
+            sys.exit()
+        
     main_display.fill(COLOR_BLACK)
 
     if player_rect.bottom >= HEIGHT:
         player_speed = [1, -1]
 
-    if player_rect.right >=  WIDTH:
+    if player_rect.right >= WIDTH:
         player_speed = [-1, -1]
 
+    if player_rect.top <= 0:
+        player_speed = [-1, 1]
+
+    if player_rect.left <= 0:
+        player_speed = [1, 1]
+    
     main_display.blit(player, player_rect)
 
     player_rect = player_rect.move(player_speed)
